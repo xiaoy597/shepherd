7dd3dd6dd47cffb61b083205e855a6053b88076f
xiaoy <xiaoy597@qq.com>
2018-10-16 23:05:30 +0800

    add windows command script for starting scrapyd.

---
+++ start-scrapyd.bat
@@ -0,0 +1,10 @@
+@SET "CONDA_DEFAULT_ENV=venv3.7"
+@SET "CONDA_EXE=c:\ProgramData\Anaconda3\Scripts\conda.exe"
+@SET "CONDA_PREFIX=C:\Users\xiaoy\AppData\Local\conda\conda\envs\venv3.7"
+@SET "CONDA_PROMPT_MODIFIER=(venv3.7) "
+@SET "CONDA_PYTHON_EXE=c:\ProgramData\Anaconda3\python.exe"
+@SET "CONDA_SHLVL=1"
+@SET "PATH=C:\Users\xiaoy\AppData\Local\conda\conda\envs\venv3.7;C:\Users\xiaoy\AppData\Local\conda\conda\envs\venv3.7\Library\mingw-w64\bin;C:\Users\xiaoy\AppData\Local\conda\conda\envs\venv3.7\Library\usr\bin;C:\Users\xiaoy\AppData\Local\conda\conda\envs\venv3.7\Library\bin;C:\Users\xiaoy\AppData\Local\conda\conda\envs\venv3.7\Scripts;C:\Users\xiaoy\AppData\Local\conda\conda\envs\venv3.7\bin;C:\Program Files\Docker\Docker\Resources\bin;C:\Program Files (x86)\Common Files\Oracle\Java\javapath;C:\ProgramData\Oracle\Java\javapath;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\Strawberry\c\bin;C:\Strawberry\perl\site\bin;C:\Strawberry\perl\bin;C:\Program Files (x86)\sbt\bin;C:\Program Files (x86)\scala\bin;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\Git LFS;C:\Program Files (x86)\Calibre2\;C:\Users\xiaoy\AppData\Local\Microsoft\WindowsApps;"
+@SET "PYTHONIOENCODING=936"
+
+scrapyd
